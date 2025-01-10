caced2be875df1d529afdc2d39d46bf851687aba
i4ox <arthurlokhov@tuta.io>
2025-01-10 18:30:49 +0300

    add icons support inside neovim

--- nvim/lua/specs/requirements-spec.lua
+++ nvim/lua/specs/requirements-spec.lua
@@ -1,3 +1,24 @@
 return {
   { "nvim-lua/plenary.nvim" },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    init = function()
+      package.preload["nvimd-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+    config = function()
+      require("mini.icons").setup {
+        file = {
+          [".keep"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        },
+        filetype = {
+          dotenv = { glyph = "", hl = "MiniIconsYellow" },
+        },
+      }
+    end,
+  },
 }
