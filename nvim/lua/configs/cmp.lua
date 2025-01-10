cc1431b06f6c874f3d8c7b0acb1dd91bf65e1501
i4ox <arthurlokhov@tuta.io>
2025-01-10 21:42:15 +0300

    add completion support for neovim

---
+++ nvim/lua/configs/cmp.lua
@@ -0,0 +1,71 @@
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { "./snippets" },
+      }
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = {
+      border = "rounded",
+      winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+    },
+    documentation = {
+      border = "rounded",
+      winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+    },
+  },
+  mapping = cmp.mapping.preset.insert {
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<Esc>"] = cmp.mapping.close(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = "buffer", keyword_length = 2 },
+  },
+  completion = {
+    keyword_length = 1,
+    completeopt = 1,
+  },
+  view = {
+    entries = "custom",
+  },
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = "symbol_text",
+      menu = {
+        nvim_lsp = "[LSP]",
+        path = "[Path]",
+        buffer = "[Buffer]",
+        emoji = "[Emoji]",
+      },
+      show_labelDetails = true,
+      maxwidth = 40,
+      ellipsis_char = "...",
+    },
+  },
+}
--- nvim/lua/specs/editor-spec.lua
+++ nvim/lua/specs/editor-spec.lua
@@ -11,4 +11,36 @@ return {
       require("configs.treesitter")
     end,
   },
+  {
+    "iguanacucumber/magazine.nvim", -- not hard fork of nvim-cmp
+    name = "nvim-cmp",
+    event = "VeryLazy",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "onsails/lspkind-nvim",
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
+    },
+    config = function()
+      require("configs.cmp")
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      autopairs.setup()
+    end,
+  },
 }
