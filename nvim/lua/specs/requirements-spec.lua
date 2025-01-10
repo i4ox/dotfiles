b18bbd183de2cd8bee402f9c295afa4b323a405c
i4ox <arthurlokhov@tuta.io>
2025-01-10 21:41:53 +0300

    fix problem with nvim-web-devicons and mini.icons

--- nvim/lua/specs/requirements-spec.lua
+++ nvim/lua/specs/requirements-spec.lua
@@ -4,7 +4,7 @@ return {
     "echasnovski/mini.icons",
     lazy = true,
     init = function()
-      package.preload["nvimd-web-devicons"] = function()
+      package.preload["nvim-web-devicons"] = function()
         require("mini.icons").mock_nvim_web_devicons()
         return package.loaded["nvim-web-devicons"]
       end
