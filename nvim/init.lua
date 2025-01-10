6737edeb01362f916437abb84418ceb64e37777e
i4ox <arthurlokhov@tuta.io>
2025-01-10 15:58:11 +0300

    add neovim initial config

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- This is my personal NeoVim configuration supporting Linux, and maybe Windows and Mac, with various plugins configured.
+-- This configuration evolves as I learn more about NeoVim and become more proficient in using NeoVim.
+-- I would not recommend cloning this configuration and replace your own config. Good configuration are personal, build over time with a lot of polish
+--
+-- Author: Arthur Lokhov
+-- Email: arthurlokhov@tuta.io
+-- GitHub: https://github.com/i4ox
+
+-- Enable lazy loading for modules
+vim.loader.enable()
