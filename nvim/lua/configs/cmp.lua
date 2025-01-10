e08e7814894874e144b82cbed42db7fc9fe31d9c
i4ox <arthurlokhov@tuta.io>
2025-01-10 21:57:53 +0300

    fix some problems with cmp

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -4,9 +4,7 @@ local lspkind = require("lspkind")
 cmp.setup {
   snippet = {
     expand = function(args)
-      require("luasnip.loaders.from_vscode").lazy_load {
-        paths = { "./snippets" },
-      }
+      require("luasnip.loaders.from_vscode").lazy_load({})
       require("luasnip").lsp_expand(args.body)
     end,
   },
@@ -40,6 +38,7 @@ cmp.setup {
     ["<Esc>"] = cmp.mapping.close(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
   },
   sources = {
     { name = "nvim_lsp" },
