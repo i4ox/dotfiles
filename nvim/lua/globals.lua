7d9a69a9fbdbb9bd9ddc898eb7312c1e8ee257fa
i4ox <arthurlokhov@tuta.io>
2025-01-10 16:12:22 +0300

    disable some neovim plugins

--- nvim/lua/globals.lua
+++ nvim/lua/globals.lua
@@ -10,3 +10,12 @@ g.maplocalleader = ","
 
 -- [[ Backup ]]
 g.backupdir = fn.expand(fn.stdpath("data") .. "/backup//")
+
+
+-- [[ Plugins ]]
+g.loaded_2html_plugin = 1 -- Do not load tohtml.vim
+g.loaded_zipPlugin = 1 -- Do not load zipPlugin.vim
+g.loaded_gzip = 1 -- Do not load gzip.vim
+g.loaded_tarPlugin = 1 -- Do not load tarPlugin.vim
+g.loaded_tutor_mode_plugin = 1 -- Do not load the tutor plugin
+g.loaded_sql_completion = 1 -- Disable sql omni completion, it is broken
