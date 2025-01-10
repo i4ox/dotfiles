47dca8345baa6a59ec304edcbf9da0de46c68cbf
i4ox <arthurlokhov@tuta.io>
2025-01-10 20:34:50 +0300

    add transparent.nvim

--- nvim/lua/specs/colorschemes-spec.lua
+++ nvim/lua/specs/colorschemes-spec.lua
@@ -1,6 +1,54 @@
 return {
   {
     "olimorris/onedarkpro.nvim",
+    dependencies = {
+      {
+        "xiyaowong/transparent.nvim",
+        opts = {
+          groups = { -- table: default groups
+            "Normal",
+            "NormalNC",
+            "Comment",
+            "Constant",
+            "Special",
+            "Identifier",
+            "Statement",
+            "PreProc",
+            "Type",
+            "Underlined",
+            "Todo",
+            "String",
+            "Function",
+            "Conditional",
+            "Repeat",
+            "Operator",
+            "Structure",
+            "LineNr",
+            "NonText",
+            "SignColumn",
+            "StatusLine",
+            "StatusLineNC",
+            "EndOfBuffer",
+          },
+          extra_groups = {
+            "NvimTreeNormal",
+            "NormalFloat",
+            "NvimTreeEndOfBuffer",
+            "StatusLine",
+            "StatusLineNC",
+            "CmpItemMenu",
+            "NoiceScrollbar",
+          }, -- table: additional groups that should be cleared
+          exclude_groups = {
+            "CursorLine",
+            "CursorLineNr",
+            "CursorColumn",
+            "InclineNormalFloat",
+            "InclineNormalFloatNC",
+          }, -- table: groups you don't want to clear,
+        },
+      },
+    },
     priority = 1000,
     config = function()
       require("onedarkpro").setup {
